47206be0c2e21023dfab53af99dab492f4a7baae
sak_shima <sak_shima@worksid.co.jp>
2021-08-17 14:47:55 +0900

    押したら乱数が表示される

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,17 @@
 <template>
-  <Tutorial/>
+  <div>
+    <!-- <Tutorial /> -->
+    <input type="text" />
+    <button @click="onClick">押したら乱数が出る</button>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  methods: {
+    onClick() {
+      alert(Math.random())
+    }
+  }
+}
 </script>
